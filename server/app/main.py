30177499d1454424be2b7e639f0c291dee82a9fb
hverified <siddiquikhalid10@gmail.com>
2025-01-05 01:04:32 +0530

    added chromium

--- server/app/main.py
+++ server/app/main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth, bank, portfolio, market, scrape_table
 from app.core.config import settings
+import os
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    os.system("apt-get update && apt-get install -y chromium")
+    yield
+    pass
+
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -16,7 +26,9 @@ app = FastAPI(
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
     docs_url=settings.DOCS_URL,
+    lifespan=lifespan,
 )
+
 
 app.add_middleware(
     CORSMiddleware,
