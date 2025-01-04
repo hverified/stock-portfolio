d67d6ccbd1bb730666b174538ab0303bcf023215
hverified <siddiquikhalid10@gmail.com>
2025-01-05 01:10:57 +0530

    added chromium

--- server/app/routes/scrape_table.py
+++ server/app/routes/scrape_table.py
@@ -17,9 +17,11 @@ class ScrapingRequest(BaseModel):
 def scrape_table_to_json(url: str, table_id: str):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.binary_location = "/usr/bin/chromium"
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
