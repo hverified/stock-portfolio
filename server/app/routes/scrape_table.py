3eac79c8f7eac8a2916392c03b3b1e63ba75929e
hverified <siddiquikhalid10@gmail.com>
2025-01-05 02:40:33 +0530

    set chromium path

--- server/app/routes/scrape_table.py
+++ server/app/routes/scrape_table.py
@@ -20,6 +20,7 @@ def scrape_table_to_json(url: str, table_id: str):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = os.environ.get("CHROME_BIN", "/usr/bin/chromium")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
