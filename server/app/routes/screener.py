c12c0498d7173329d51ff245b11b82b34375e290
hverified <siddiquikhalid10@gmail.com>
2025-02-05 21:00:11 +0530

    updated cron time

--- server/app/routes/screener.py
+++ server/app/routes/screener.py
@@ -2,23 +2,33 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 import json
 from datetime import datetime
+import os
 
 router = APIRouter()
 
-# Define the path to your JSON file
 json_file_path = "stock_screener.json"
 
-# Read the JSON file when the app starts
 def read_stock_screener_data():
+    if not os.path.exists(json_file_path):
+        return []
+
     with open(json_file_path, "r") as file:
-        data = json.load(file)
+        try:
+            if os.stat(json_file_path).st_size == 0:
+                return []
+            data = json.load(file)
+        except json.JSONDecodeError:
+            return []
     return data
 
 @router.get("/stocks")
 async def get_stock_screener_data():
     stock_data = read_stock_screener_data()
 
-    # Sort the stock data by the "date" field in descending order
-    sorted_stock_data = sorted(stock_data, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d'), reverse=True)
+    sorted_stock_data = sorted(
+        stock_data, 
+        key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d'), 
+        reverse=True
+    ) if stock_data else []
 
     return JSONResponse(content=sorted_stock_data)
