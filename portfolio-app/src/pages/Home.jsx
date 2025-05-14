57c2ed6b07f445e53c6aa025c622ed5bf9e67ae4
hverified <siddiquikhalid10@gmail.com>
2025-05-14 16:32:10 +0530

    Enhanced UI

--- portfolio-app/src/pages/Home.jsx
+++ portfolio-app/src/pages/Home.jsx
@@ -5,11 +5,11 @@ import MarketCard from "../components/MarketCard";
 
 const Home = () => {
     return (
-        <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 p-6 pb-20">
+        <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 p-6">
             <div className="max-w-6xl mx-auto space-y-8 animate-fade-in">
                 <Header />
                 <MarketCard />
-                <PortfolioCard />
+                {/* <PortfolioCard /> */}
             </div>
         </div>
     );
